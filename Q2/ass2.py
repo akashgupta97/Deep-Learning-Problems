30f071b1de12177d8d098c49f04180a663629f32
Akash <akash.darknight@gmail.com>
2018-10-20 16:37:00 +0530

    Handwritten Digits Recognizer using Tensorflow

---
+++ Q2/ass2.py
@@ -0,0 +1,9 @@
+"""Convolutional Neural Network Estimator for MNIST, built with tf.layers."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import numpy as np
+import tensorflow as tf
+
