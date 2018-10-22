8d9ac4131cdc06cd0576fad1810d8ba442dfeb6c
Akash <akash.darknight@gmail.com>
2018-10-22 16:37:00 +0530

    Handwritten Digits Recognizer using Tensorflow

--- Q2/ass2.py
+++ Q2/ass2.py
@@ -7,3 +7,19 @@ from __future__ import print_function
 import numpy as np
 import tensorflow as tf
 
+
+tf.logging.set_verbosity(tf.logging.INFO)
+
+
+def cnn_model_fn(features, labels, mode):
+  """Model function for CNN."""
+  # Input Layer
+  # Reshape X to 4-D tensor: [batch_size, width, height, channels]
+  # MNIST images are 28x28 pixels, and have one color channel
+  input_layer = tf.reshape(features["x"], [-1, 28, 28, 1])
+  # Convolutional Layer #1
+  # Computes 32 features using a 5x5 filter with ReLU activation.
+  # Padding is added to preserve width and height.
+  # Input Tensor Shape: [batch_size, 28, 28, 1]
+  # Output Tensor Shape: [batch_size, 28, 28, 32]
+  conv1 = tf.layers.conv2d(inputs=input_layer,filters=32,kernel_size=[5, 5],padding="same",activation=tf.nn.relu)
