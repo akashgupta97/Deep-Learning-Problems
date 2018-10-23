d34a92f1639901a6ebf6a81d4c370f9320016721
Akash <akash.darknight@gmail.com>
2018-10-23 16:37:00 +0530

    Handwritten Digits Recognizer using Keras

---
+++ Q1/ass1.py
@@ -0,0 +1,16 @@
+'''Trains a simple convnet on the MNIST dataset.
+
+Gets to 99.25% test accuracy after 12 epochs
+(there is still a lot of margin for parameter tuning).
+16 seconds per epoch on a GRID K520 GPU.
+'''
+
+from __future__ import print_function
+import keras
+import matplotlib.pyplot as plt
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras import backend as K
+
