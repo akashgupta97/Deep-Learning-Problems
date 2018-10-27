9c7072a1cb286e45ca412c19b8cb14e2b475b01f
Akash <akash.darknight@gmail.com>
2018-10-27 16:37:00 +0530

    Handwritten Digits Recognizer using Keras

--- Q1/ass1.py
+++ Q1/ass1.py
@@ -33,3 +33,14 @@ else:
     x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
     input_shape = (img_rows, img_cols, 1)
 
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train /= 255
+x_test /= 255
+print('x_train shape:', x_train.shape)
+print(x_train.shape[0], 'train samples')
+print(x_test.shape[0], 'test samples')
+
+# convert class vectors to binary class matrices
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
