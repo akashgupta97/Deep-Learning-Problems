996ccb53a4cf934c9c2860ab0a59ee621ee1e0a3
Akash <akash.darknight@gmail.com>
2018-10-30 16:37:00 +0530

    Handwritten Digits Recognizer using Tensorflow

--- Q2/ass2.py
+++ Q2/ass2.py
@@ -75,3 +75,19 @@ def cnn_model_fn(features, labels, mode):
       "accuracy": tf.metrics.accuracy(labels=labels, predictions=predictions["classes"])}
   return tf.estimator.EstimatorSpec(mode=mode, loss=loss, eval_metric_ops=eval_metric_ops)
 
+def main(unused_argv):
+  # Load training and eval data
+  mnist = tf.contrib.learn.datasets.load_dataset("mnist")
+  train_data = mnist.train.images  # Returns np.array
+  train_labels = np.asarray(mnist.train.labels, dtype=np.int32)
+  eval_data = mnist.test.images  # Returns np.array
+  eval_labels = np.asarray(mnist.test.labels, dtype=np.int32)
+  # Create the Estimator
+  mnist_classifier = tf.estimator.Estimator(
+      model_fn=cnn_model_fn, model_dir="/tmp/mnist_convnet_model")
+  # Set up logging for predictions
+  # Log the values in the "Softmax" tensor with label "probabilities"
+  tensors_to_log = {"probabilities": "softmax_tensor"}
+  logging_hook = tf.train.LoggingTensorHook(
+      tensors=tensors_to_log, every_n_iter=50)
+
