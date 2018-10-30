4dc18655b0127e6f6e57776a260ebd71e012fcda
Akash <akash.darknight@gmail.com>
2018-10-30 16:37:00 +0530

    Character Level - Language Model

--- Q3/ass3.py
+++ Q3/ass3.py
@@ -3,3 +3,9 @@
 import tensorflow as tf
 import matplotlib.pyplot as plt
 from tensorlm import Vocabulary, Dataset, GeneratingLSTM
+
+
+TEXT_PATH = "C:/ass1/q3/tinytrain.txt"
+DEV_PATH = "C:/ass1/q3/tinyvalid.txt"
+BATCH_SIZE = 20
+NUM_TIMESTEPS = 30
