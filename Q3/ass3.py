adf61ae5260f9f48821fa4b08d37ba753a866d8d
Akash <akash.darknight@gmail.com>
2018-11-03 16:37:00 +0530

    Character Level - Language Model

--- Q3/ass3.py
+++ Q3/ass3.py
@@ -9,3 +9,17 @@ TEXT_PATH = "C:/ass1/q3/tinytrain.txt"
 DEV_PATH = "C:/ass1/q3/tinyvalid.txt"
 BATCH_SIZE = 20
 NUM_TIMESTEPS = 30
+
+with tf.Session() as session:
+    # Generate a vocabulary based on the text
+    vocab = Vocabulary.create_from_text(TEXT_PATH, max_vocab_size=96, level="char")
+    # Obtain input and target batches from the text file
+    dataset = Dataset(TEXT_PATH, vocab, BATCH_SIZE, NUM_TIMESTEPS)
+    # Create the model in a TensorFlow graph
+    model = GeneratingLSTM(vocab_size=vocab.get_size(),
+                           neurons_per_layer=100,
+                           num_layers=2,
+                           max_batch_size=BATCH_SIZE,
+                           output_keep_prob=0.8)
+    # Initialize all defined TF Variables
+    
