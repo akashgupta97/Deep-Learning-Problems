101d76fe8bcf47d9bebc8c39777b4bdece1c56e7
Akash <akash.darknight@gmail.com>
2019-01-13 16:37:00 +0530

    Image Reconstruction using variational auto-encoder using Keras

--- Q6/ass6.py
+++ Q6/ass6.py
@@ -20,3 +20,21 @@ train_orig = pd.read_csv('C:/ass1/train.csv')
 test_orig = pd.read_csv('C:/ass1/test.csv')
 
 train_orig.head()
+
+
+# create 'label' column in test dataset; rearrange so that columns are in the same order as in train
+test_orig['label'] = 11
+testCols = test_orig.columns.tolist()
+testCols = testCols[-1:] + testCols[:-1]
+test_orig = test_orig[testCols]
+
+# combine original train and test sets
+combined = pd.concat([train_orig, test_orig], ignore_index = True)
+
+combined.head()
+
+combined.tail()
+# Hold out 5000 random images as a validation/test sample
+valid = combined.sample(n = 5000, random_state = 555)
+train = combined.loc[~combined.index.isin(valid.index)]
+
