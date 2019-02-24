25cdd8e50d979dbc13dadc4e97a3ec9b84939e85
Akash <akash.darknight@gmail.com>
2019-02-24 16:37:00 +0530

    Image Reconstruction using Variational Autoencoder and Tensorflow

--- Q7/untitled.py
+++ Q7/untitled.py
@@ -226,3 +226,41 @@ def train(network_architecture, learning_rate=0.001,
             print("Epoch:", '%04d' % (epoch+1),
                   "cost=", "{:.9f}".format(avg_cost))
     return vae
+
+if __name__=="__main__":
+	network_architecture = \
+	    dict(n_hidden_recog_1=500, # 1st layer encoder neurons
+		 n_hidden_recog_2=500, # 2nd layer encoder neurons
+		 n_hidden_gener_1=500, # 1st layer decoder neurons
+		 n_hidden_gener_2=500, # 2nd layer decoder neurons
+		 n_input=784, # MNIST data input (img shape: 28*28)
+		 n_z=20)  # dimensionality of latent space
+
+	vae = train(network_architecture, training_epochs=75)
+
+	x_sample = mnist.test.next_batch(100)[0]
+	x_reconstruct = vae.reconstruct(x_sample)
+
+	plt.figure( )
+
+	for i in range(5):
+	    print(i)
+	    plt.subplot(5, 2, 2*i + 1)
+	    plt.imshow(x_sample[i].reshape(28, 28), vmin=0, vmax=1, cmap="gray")
+	    plt.title("Test input")
+	    plt.colorbar()
+	    plt.subplot(5, 2, 2*i + 2)
+	    plt.imshow(x_reconstruct[i].reshape(28, 28), vmin=0, vmax=1, cmap="gray")
+	    plt.title("Reconstruction")
+	    plt.colorbar()
+	plt.tight_layout()
+	plt.show()
+	network_architecture = \
+	    dict(n_hidden_recog_1=500, # 1st layer encoder neurons
+		 n_hidden_recog_2=500, # 2nd layer encoder neurons
+		 n_hidden_gener_1=500, # 1st layer decoder neurons
+		 n_hidden_gener_2=500, # 2nd layer decoder neurons
+		 n_input=784, # MNIST data input (img shape: 28*28)
+		 n_z=2)  # dimensionality of latent space
+
+	vae_2d = train(network_architecture, training_epochs=75)
