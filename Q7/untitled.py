750139d1981feeed1889de33b0c5873d8e0e0a25
Akash <akash.darknight@gmail.com>
2019-02-26 16:37:00 +0530

    Image Reconstruction using Variational Autoencoder and Tensorflow

--- Q7/untitled.py
+++ Q7/untitled.py
@@ -264,3 +264,34 @@ if __name__=="__main__":
 		 n_z=2)  # dimensionality of latent space
 
 	vae_2d = train(network_architecture, training_epochs=75)
+
+
+
+	x_sample, y_sample = mnist.test.next_batch(5000)
+	z_mu = vae_2d.transform(x_sample)
+	plt.figure(figsize=(8, 6))
+	plt.scatter(z_mu[:, 0], z_mu[:, 1], c=np.argmax(y_sample, 1))
+	plt.colorbar()
+	plt.grid()
+
+	nx = ny = 20
+	x_values = np.linspace(-3, 3, nx)
+	y_values = np.linspace(-3, 3, ny)
+
+	canvas = np.empty((28*ny, 28*nx))
+	for i, yi in enumerate(x_values):
+	    for j, xi in enumerate(y_values):
+		z_mu = np.array([[xi, yi]]*vae.batch_size)
+		x_mean = vae_2d.generate(z_mu)
+		canvas[(nx-i-1)*28:(nx-i)*28, j*28:(j+1)*28] = x_mean[0].reshape(28, 28)
+
+	plt.figure(figsize=(8, 10))
+	Xi, Yi = np.meshgrid(x_values, y_values)
+	plt.imshow(canvas, origin="upper", cmap="gray")
+	plt.tight_layout()
+	plt.show()
+
+
+
+#%load_ext watermark
+#%watermark -a "Jan Hendrik Metzen" -d -v -m -p numpy,scikit-learn,tensorflow
