75531c1285dc7ec02e570f6a54c097e463959166
Akash <akash.darknight@gmail.com>
2019-03-04 16:37:00 +0530

    Model

--- Q8/Model.py
+++ Q8/Model.py
@@ -9,3 +9,11 @@ import sys, datetime, pickle, os, re
 from Recurrent_Unit import GRU
 from Optimization import Adam, rmsprop
 from util import init_weight
+class RNN_Model:
+    def __init__(self, I=None, H=None, rnn_unit=GRU, opt=Adam, activation=T.nnet.elu):
+        self.D = I
+        self.hidden_layer_sizes = H
+        self.rnn_unit = rnn_unit
+        self.activation = activation
+        self.opt = opt
+        self.__setstate__()
