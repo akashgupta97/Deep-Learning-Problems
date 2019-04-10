b71c9fe20cf3b37e8ceae70b79d73498bf032175
Akash <akash.darknight@gmail.com>
2019-04-10 16:37:00 +0530

    Prediction results

--- Q8/char_predict.py
+++ Q8/char_predict.py
@@ -5,3 +5,15 @@ def predict_chars(text, model, map_vect, i2c_map, n=2):
     out = model.forward_pass(x)
     pred = np.argsort(out[-1], axis=0)[-n:,0].tolist()
     return {i2c_map[i]:out[-1][i] for i in pred}
+
+def get_top_words(words, n):
+    a = [words[i] for i in words]
+    a.sort(reverse=True)
+    tword = []
+    itr=0
+    while itr<n and itr<len(a):
+        for j in words:
+            if words[j] == a[itr]:
+                tword.append(j)
+        itr += 1
+    return tword
