c4ba7412bf3d81b0955e5f4bfe1dc24828b474ee
Akash <akash.darknight@gmail.com>
2019-04-17 16:37:00 +0530

    Gated Recurrent Units

--- Q8/GRU.py
+++ Q8/GRU.py
@@ -8,3 +8,13 @@ class gru:
 
         self.optimize = optimize
         self.names = {0:'ur',1:'wr', 2:'uz', 3:'wz', 4:'u_h', 5:'w_h', 6:'wo'}
+        if wb:
+            self.w, self.b = self.load_model(wb)
+            self.h_size, self.i_size= self.w['ur'].shape
+            self.o_size, _= self.w['wo'].shape
+        else:
+            self.i_size = i_size
+            self.h_size = h_size
+            self.o_size = o_size
+            self.w={}
+            self.b={}
