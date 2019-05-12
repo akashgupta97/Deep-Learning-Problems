dd8c9a4197a084dfa6dff6ae2bbb7d25669d4817
Akash <akash.darknight@gmail.com>
2019-05-12 16:37:00 +0530

    Main File

--- Q8/main.py
+++ Q8/main.py
@@ -4,3 +4,10 @@ from GRU import gru
 from vanilla_rnn import vrnn
 from rnn_interface import train, test
 
+def usage():
+    print "Usage : python main.py [options]"
+    print "options:"
+    print "\t -rnn vrnn or gru: define type recurrent model use (by default 'gru')"
+    print "\t -text file name: input file name use for trainning model for character prediction"
+    print "\t -npredict number of pridection: given npredict number of words predicted"
+    exit()
