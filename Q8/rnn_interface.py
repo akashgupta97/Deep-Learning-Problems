c04db2c172079d2ffc5a9fbca0155500dda4ad2d
Akash <akash.darknight@gmail.com>
2019-05-29 16:37:00 +0530

    RNN

--- Q8/rnn_interface.py
+++ Q8/rnn_interface.py
@@ -8,3 +8,8 @@ def sample_formation(text, seq_length, map_vect):
         y = [map_vect[j] for j in text[i + 1: i + seq_length + 1]]
         samples.append((x, y))
     return samples
+
+def train(fname, rnn, map_vect):
+    text = open(fname,'r').read()
+    chars = list(set(text))
+    v_size, t_size = len(chars), len(text)
