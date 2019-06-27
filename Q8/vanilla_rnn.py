3efd474c088468614d0f49e5e86d291dd6c53a80
Akash <akash.darknight@gmail.com>
2019-06-27 16:37:00 +0530

    Vanilla RNN

--- Q8/vanilla_rnn.py
+++ Q8/vanilla_rnn.py
@@ -4,3 +4,10 @@ from util import softmax, sigmoid, dsigmoid, adam, rmsprop
 import pickle
 
 class vrnn:
+
+    def __init__(self, i_size, h_size, o_size, optimize='rmsprop', wb=None):
+
+        self.i_size = i_size
+        self.h_size = h_size
+        self.o_size = o_size
+        self.optimize = optimize
