4bdbd6136323fda7890b86fe44b11434482f7d7b
Akash <akash.darknight@gmail.com>
2019-06-28 16:37:00 +0530

    Vanilla RNN

--- Q8/vanilla_rnn.py
+++ Q8/vanilla_rnn.py
@@ -11,3 +11,9 @@ class vrnn:
         self.h_size = h_size
         self.o_size = o_size
         self.optimize = optimize
+
+        if wb:
+            self.w, self.b = self.load_model(wb)
+        else:
+            self.w={}
+            self.b={}
